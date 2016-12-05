7697b1eb539ecc3b9e6e5acdcb974ccf5271c1fb
djbieber <danbieber1991@gmail.com>
2016-12-05 17:15:05 -0500

    first commit - adding template files for documentation

---
+++ docs/build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({envversion:46,filenames:["index"],objects:{},objnames:{},objtypes:{},terms:{come:0,content:0,first:0,index:0,line:0,modul:0,more:0,page:0,search:0,thi:0},titles:["Welcome to Signals User Guide&#8217;s documentation!"],titleterms:{document:0,guid:0,indic:0,signal:0,tabl:0,user:0,welcom:0}})
